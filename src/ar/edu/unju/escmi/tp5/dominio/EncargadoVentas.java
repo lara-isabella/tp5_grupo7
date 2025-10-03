41af691c57096d33dd4c9bdee8d8695bb6851681
lulipereira <lulipereira2006@gmail.com>
2025-10-03 02:12:57 -0300

    Correciones

--- src/ar/edu/unju/escmi/tp5/dominio/EncargadoVentas.java
+++ src/ar/edu/unju/escmi/tp5/dominio/EncargadoVentas.java
@@ -1,5 +1,16 @@
 package ar.edu.unju.escmi.tp5.dominio;
 
-public class EncargadoVentas {
+public class EncargadoVentas extends Empleados {
 
+    public EncargadoVentas(String nombre, String apellido, String direccion) {
+        super(nombre, apellido, direccion);
+    }
+
+    @Override
+    public void mostrarMenu() {
+        System.out.println("=== Menú Encargado de Ventas ===");
+        System.out.println("1. Mostrar todas las ventas");
+        System.out.println("2. Mostrar total de ventas");
+        System.out.println("3. Verificar stock de producto");
+    }
 }
--- src/ar/edu/unju/escmi/tp5/dominio/LineaFactura.java
+++ src/ar/edu/unju/escmi/tp5/dominio/LineaFactura.java
@@ -1,5 +1,18 @@
 package ar.edu.unju.escmi.tp5.dominio;
 
 public class LineaFactura {
+    private Producto producto;
+    private int cantidad;
 
+    public LineaFactura(Producto producto, int cantidad) {
+        this.producto = producto;
+        this.cantidad = cantidad;
+    }
+
+    public double calcularImporte() {
+        return cantidad * producto.getPrecioUnitario();
+    }
+
+    public Producto getProducto() { return producto; }
+    public int getCantidad() { return cantidad; }
 }
